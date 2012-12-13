3682dbd7b34875c27b9f011c93a261087dcccfb2
Felix Enescu <f@qsol.ro>
2012-12-13 11:06:47 +0200

    memcached and nginx.

--- pnp4nagios_templates/check_mk-memcached.requests.php
+++ pnp4nagios_templates/check_mk-memcached.requests.php
@@ -16,36 +16,33 @@ $idx = 1;
 $opt[$idx] = "--vertical-label 'Requests' -b1000 -l0 --title \"Memcached Requests for $hostname\" ";
 $def[$idx] =  "DEF:get=$RRDFILE[1]:$DS[1]:AVERAGE " ;
 $def[$idx] .=  "DEF:set=$RRDFILE[2]:$DS[2]:AVERAGE " ;
+$def[$idx] .= "CDEF:neg_set=set,-1,* ";
 $def[$idx] .=  "DEF:misses=$RRDFILE[3]:$DS[3]:AVERAGE " ;
 $def[$idx] .=  "DEF:hits=$RRDFILE[4]:$DS[4]:AVERAGE " ;
 $def[$idx] .= "CDEF:total=get,set,+ ";
 
-$def[$idx] .= "AREA:set#ff6000:\"Cmd Sets\" " ;
+$def[$idx] .= "AREA:get#60f020:\"Cmd Gets\" " ;
+$def[$idx] .= "LINE:get#004080 " ;
 
-$def[$idx] .= "GPRINT:set:LAST:\"%9.2lf%S last\" " ;
-$def[$idx] .= "GPRINT:set:AVERAGE:\"%9.2lf%S avg\" " ;
-$def[$idx] .= "GPRINT:set:MAX:\"%9.2lf%S max\\n\" ";
-
-$def[$idx] .= "AREA:get#60f020:\"Cmd Gets\":STACK " ;
 
 $def[$idx] .= "GPRINT:get:LAST:\"%9.2lf%S last\" " ;
 $def[$idx] .= "GPRINT:get:AVERAGE:\"%9.2lf%S avg\" " ;
 $def[$idx] .= "GPRINT:get:MAX:\"%9.2lf%S max\\n\" ";
 
-$def[$idx] .= "LINE:total#004080:\"Total   \" " ;
-$def[$idx] .= "GPRINT:total:LAST:\"%9.2lf%S last\" " ;
-$def[$idx] .= "GPRINT:total:AVERAGE:\"%9.2lf%S avg\" " ;
-$def[$idx] .= "GPRINT:total:MAX:\"%9.2lf%S max\\n\" ";
+$def[$idx] .= "AREA:neg_set#ff6000:\"Cmd Sets\" " ;
+$def[$idx] .= "LINE:neg_set#004080 " ;
+
+$def[$idx] .= "GPRINT:set:LAST:\"%9.2lf%S last\" " ;
+$def[$idx] .= "GPRINT:set:AVERAGE:\"%9.2lf%S avg\" " ;
+$def[$idx] .= "GPRINT:set:MAX:\"%9.2lf%S max\\n\" ";
+
 
 $def[$idx] .= "LINE:misses#ff0000:\"Misses  \" " ;
 $def[$idx] .= "GPRINT:misses:LAST:\"%9.2lf%S last\" " ;
 $def[$idx] .= "GPRINT:misses:AVERAGE:\"%9.2lf%S avg\" " ;
 $def[$idx] .= "GPRINT:misses:MAX:\"%9.2lf%S max\\n\" ";
 
-$def[$idx] .= "LINE:hits#0000ff:\"Hits    \" " ;
-$def[$idx] .= "GPRINT:hits:LAST:\"%9.2lf%S last\" " ;
-$def[$idx] .= "GPRINT:hits:AVERAGE:\"%9.2lf%S avg\" " ;
-$def[$idx] .= "GPRINT:hits:MAX:\"%9.2lf%S max\\n\" ";
+$def[$idx] .= "LINE:0#000000 " ;
 
 $idx++;
 $opt[$idx] = "--vertical-label '%' -b1000 -l0 --title \"Memcached Hit Ratio for $hostname\" ";
